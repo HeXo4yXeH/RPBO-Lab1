d2d5405e1652a0c6aab4c1992e1e754a28211350
HeXo4yXa <nikita099900@mail.ru>
2023-09-24 23:38:31 +0300

    Добавьте файлы проекта.

---
+++ RPBO Lab1.2/ReadPersonData.cpp
@@ -0,0 +1,7 @@
+#include "Header.h"
+
+void ReadPersonData(unsigned short& age, string& name, double& salary) {
+    age = ReadPersonAge();
+    name = ReadPersonName();
+    ReadPersonSalary(&salary);
+}
